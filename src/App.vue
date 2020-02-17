7b55666b3c6cb89a3b81bd035c81218a5ba85de0
facundo <facundovigo@gmail.com>
2020-02-17 10:45:24 -0300

    Adding files.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="SAMI FUTURO SISTEMA DE TURNOS ODONTOLOGICO"/>
   </div>
 </template>
 
